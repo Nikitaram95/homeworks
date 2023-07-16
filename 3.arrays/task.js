8645a0dcc7bc1423098f2eb0afd1fdfcb961e04c
Nikita Kuznetsov <iki19955@yandex.ru>
2023-07-16 13:00:22 +0300

    Задание выполено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+  return arr2.every((element, index) => element === arr1 [index]);
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const usersGender = users.filter ((user) => user.gender === gender);
+  const usersAge = usersGender.map((user) => user.age);
+  const allAge = usersAge.reduce((acc, age) => acc + age, 0);
+  if (usersAge.length === 0) {
+    return 0;
+  }
+    return allAge / usersAge.length;
 }
